79953aef103f1eb2ba68850966388f3ea636643d
Hey-sayiwanna <guxusi@gmail.com>
2026-06-28 21:39:38 +0800

    Update QQSportsMatchWidgetsAdBlock.js

--- QQSportsMatchWidgetsAdBlock.js
+++ QQSportsMatchWidgetsAdBlock.js
@@ -1,4 +1,4 @@
-let body;
+ let body;
 
 try {
   body = JSON.parse($response.body);
@@ -6,13 +6,25 @@ try {
   const data = body?.data;
   let removedCount = 0;
 
+  /*
+   * 比赛页右下角会员横幅
+   */
   if (data && Array.isArray(data.bannerList)) {
-    removedCount = data.bannerList.length;
+    removedCount += data.bannerList.length;
     data.bannerList = [];
   }
 
+  /*
+   * 直播间进入广告 / 飘窗广告
+   * 字段里会带 logo: "广告"、lottieUrl、picUrl
+   */
+  if (data && Array.isArray(data.enterAdList)) {
+    removedCount += data.enterAdList.length;
+    data.enterAdList = [];
+  }
+
   console.log(
-    `QQSports Match Widgets Ad Block: removed ${removedCount} VIP banner item(s).`
+    `QQSports Match Widgets Ad Block: removed ${removedCount} ad item(s).`
   );
 
   $done({
